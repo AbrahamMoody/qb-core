8ad152540c8e81fae65d6ff027d874ea5ba86828
AmericanLife <52460339+AbrahamMoody@users.noreply.github.com>
2024-02-25 18:56:28 +0100

    Update de.lua
    Missed Phrases

--- locale/de.lua
+++ locale/de.lua
@@ -1,13 +1,13 @@
 local Translations = {
     error = {
-        not_online = 'Der Spieler ist nicht online',
+        not_online = 'Der Bürger ist nicht erreichbar',
         wrong_format = 'Falsches Format',
         missing_args = 'Nicht alle Argumente wurden ausgefüllt (x, y, z)',
         missing_args2 = 'Alle Argumente müssen ausgefüllt sein!',
         no_access = 'Kein Zugriff auf diesen Befehl',
-        company_too_poor = 'Dein Arbeitgeber hat kein Geld mehr',
+        company_too_poor = 'Dein Gehalt konnte nicht überwießen werden',
         item_not_exist = 'Das Item existiert nicht',
-        too_heavy = 'Inventar zu voll',
+        too_heavy = 'Du trägst zuviel bei Dir.',
         location_not_exist = 'Der Ort existiert nicht',
         duplicate_license = 'Doppelte Rockstar-Lizenz gefunden',
         no_valid_license  = 'Keine verifizierte Rockstar-Lizenz gefunden',
@@ -17,6 +17,8 @@ local Translations = {
         no_permission = 'Du hast keine Rechte dafür..',
         no_waypoint = 'Kein Wegpunkt gesetzt.',
         tp_error = 'Error beim teleportieren.',
+        connecting_database_error   = '[QBCORE] - Bei der Verbindung zum Server ist ein Datenbankfehler aufgetreten. (Datenbank an??)',
+        connecting_database_timeout = '[QBCORE] - Zeitüberschreitung bei der Verbindung zur Datenbank.(Datenbank an?)',
     },
     success = {
         server_opened = 'Der Server wurde geöffnet',
@@ -24,11 +26,11 @@ local Translations = {
         teleported_waypoint = 'Zum Wegpunkt teleportiert.',
     },
     info = {
-        received_paycheck = 'Du hast dein Gehalt in Höhe von $%{value} erhalten',
-        job_info = 'Beruf: %{value} | Dienstgrad: %{value2} | im Dienst: %{value3}',
+        received_paycheck = 'Dein Gehalt in Höhe von $%{value} wurde überwießen',
+        job_info = 'Beruf: %{value} | Postion: %{value2} | im Dienst: %{value3}',
         gang_info = 'Gang: %{value} | Rang: %{value2}',
-        on_duty = 'Du befindest dich nun im Dienst!',
-        off_duty = 'Du befindest dich nun nicht mehr im Dienst!',
+        on_duty = 'Du hast eingestempelt!',
+        off_duty = 'Du hast ausgestempelt!',
         checking_ban = 'Hallo %s. Wir prüfen, ob du gebannt wurdest.',
         join_server = 'Willkommen %s bei {Server Name}.',
         checking_whitelisted = 'Hallo %s. Wir prüfen deine Erlaubnis.',
@@ -37,9 +39,9 @@ local Translations = {
     },
     command = {
         tp = {
-            help = 'TP zu Spieler oder Coords (Nur Admins)',
+            help = 'TP zu Bürger oder Coords (Nur Admins)',
             params = {
-                x = { name = 'id/x', help = 'ID vom Spieler oder X position'},
+                x = { name = 'id/x', help = 'ID vom Bürger oder X position'},
                 y = { name = 'y', help = 'Y position'},
                 z = { name = 'z', help = 'Z position'},
             },
@@ -47,16 +49,16 @@ local Translations = {
         tpm = { help = 'TP zum Marker (Nur Admins)' },
         togglepvp = { help = 'Schalte PVP ein oder aus (Nur Admins)' },
         addpermission = {
-            help = 'Gebe einem Spieler Rechte (God Only)',
+            help = 'Gebe einem Bürger Rechte (God Only)',
             params = {
-                id = { name = 'id', help = 'ID des Spielers' },
+                id = { name = 'id', help = 'ID des Bürgers' },
                 permission = { name = 'permission', help = 'Zugriffsrechte' },
             },
         },
         removepermission = {
             help = 'Nimm jemand die Rechte (God Only)',
             params = {
-                id = { name = 'id', help = 'ID des Spielers' },
+                id = { name = 'id', help = 'ID des Bürgers' },
                 permission = { name = 'permission', help = 'Zugriffsrechte' },
             },
         },
@@ -74,36 +76,39 @@ local Translations = {
             },
         },
         dv = { help = 'Fahrzeug entfernen (Nur Admins)' },
+        dvall = { help = 'Alle Fahrzeuge löschen (nur Admin)' },
+        dvp = { help = 'Alle PEDS löschen (Admin Only)' },
+        dvo = { help = 'Alle Objekte löschen (Admin Only)' },
         givemoney = {
             help = 'Gib jemandem Geld (Nur Admins)',
             params = {
-                id = { name = 'id', help = 'Spieler ID' },
+                id = { name = 'id', help = 'Bürger ID' },
                 moneytype = { name = 'moneytype', help = 'Geldtyp (Bargeld, Bank, Crypto)' },
                 amount = { name = 'amount', help = 'Geldmenge' },
             },
         },
         setmoney = {
-            help = 'Setze die Geldmenge für einen Spieler (Nur Admins)',
+            help = 'Setze die Geldmenge für einen Bürger (Nur Admins)',
             params = {
-                id = { name = 'id', help = 'Spieler ID' },
+                id = { name = 'id', help = 'Bürger ID' },
                 moneytype = { name = 'moneytype', help = 'Geldtyp (Bargeld, Bank, Crypto)' },
                 amount = { name = 'amount', help = 'Geldmenge' },
             },
         },
         job = { help = 'Check deinen Job' },
         setjob = {
-            help = 'Setze den Job eines Spielers (Nur Admins)',
+            help = 'Setze den Job eines Bürgers (Nur Admins)',
             params = {
-                id = { name = 'id', help = 'Spieler ID' },
+                id = { name = 'id', help = 'Bürger ID' },
                 job = { name = 'job', help = 'Job Name' },
                 grade = { name = 'grade', help = 'Dienstgrad' },
             },
         },
         gang = { help = 'Check deine Gang' },
         setgang = {
-            help = 'Setze die Gang eines Spielers (Nur Admins)',
+            help = 'Setze die Gang eines Bürgers (Nur Admins)',
             params = {
-                id = { name = 'id', help = 'Spieler ID' },
+                id = { name = 'id', help = 'Bürger ID' },
                 gang = { name = 'gang', help = 'Gang Name' },
                 grade = { name = 'grade', help = 'Gang Rang' },
             },
